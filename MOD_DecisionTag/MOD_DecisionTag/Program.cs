315adbde05f648fabaa9c51fa298463df30a96c2
chentaichen <ct.chen@ischool.com.tw>
2016-02-01 15:30:55 +0800

    校長模組。

---
+++ MOD_DecisionTag/MOD_DecisionTag/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FISCA;
+using Customization.Tagging;
+using System.Drawing;
+
+
+namespace MOD_DecisionTag
+{
+    public class Program
+    {
+        [MainMethod()]
+        public static void Main()
+        {
+            SystemTag.Define("Teacher", "角色:校長", Color.Purple, "Role.0001", "設定「校長」", "校長");
+        }        
+    }
+}
